229f364c311576ae3da05fb2e16f781ccf82eed6
Yeazzing <yazin71@gmail.com>
2023-09-17 18:41:05 +0900

    rech5

--- ch/ch5.py
+++ ch/ch5.py
@@ -254,7 +254,7 @@ class TwoLayerNet:
         grads['W1'] = self.layers['Affine1'].dW
         grads['b1'] = self.layers['Affine1'].db
         grads['W2'] = self.layers['Affine2'].dW
-        grads['b2'] = self.layers['Affine2'].dW
+        grads['b2'] = self.layers['Affine2'].db
         return grads
 
 #기울기 검증
